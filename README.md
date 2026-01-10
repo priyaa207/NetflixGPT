a17858128b9e49899b2bf7c18d7074f4383fdc62
priyaa207 <priyaa207@users.noreply.github.com>
2026-01-11 00:11:14 +0530

    created Login form

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 #Features
-- 
+- create-react-app
+- tailwind css
+- Login page
+ - sign in  & sign up
---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+const Header = () => {
+  return (
+    <div className="absolute w-56  px-8 pt-6 bg-gradient-to-b from-black to-transparent">
+        <img src="https://help.nflxext.com/helpcenter/OneTrust/oneTrust_production_2026-01-09/consent/87b6a5c0-0104-4e96-a291-092c11350111/019ae4b5-d8fb-7693-90ba-7a61d24a8837/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
+        alt="logo" />
+    </div>
+  )
+}
+
+export default Header
